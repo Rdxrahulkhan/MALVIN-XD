ee1ee1de107f3c78810323fd4f57fe64354e4c84
Rdxrahulkhan <164171774+Rdxrahulkhan@users.noreply.github.com>
2025-07-28 17:32:41 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~xntgHYqT#WHgl6GzkFlNZt0kzFWjVBOVyrQxTG9RGwgBwbAYS1F8",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "93770603875", // PUT OWNER RDX RAHUL VAI 
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
